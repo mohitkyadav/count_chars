d5f858766bc6d31732029ac1ee9e8726d08fcec2
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-05-21 14:24:00 -0700

    add center card

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: white;
 }
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,25 +1,45 @@
 <template>
   <div class="landing">
     <v-card flat>
-      <v-toolbar dark color="blue" flat extended>
+      <v-toolbar dark color="blue" class="elevation-3" flat>
         <v-toolbar-side-icon></v-toolbar-side-icon>
       </v-toolbar>
       <v-layout row pb-2>
-        <v-flex xs8 offset-xs2>
-          <v-card class="card--flex-toolbar">
-            <v-toolbar color="grey darken-4" card prominent>
+        <v-flex xs10 offset-xs1>
+          <v-card class="card--flex-toolbar elevation-16">
+            <v-toolbar color="grey darken-3" card prominent>
               <v-toolbar-title class="body-2 grey--text">Paste your text here</v-toolbar-title>
               <v-spacer></v-spacer>
               <v-btn icon>
-                <v-icon>search</v-icon>
+                <v-icon color="blue darken1">search</v-icon>
               </v-btn>
             </v-toolbar>
             <v-divider></v-divider>
-            <v-card-text style="height: 200px;"></v-card-text>
+            <v-card-text style="height:400px;"></v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
     </v-card>
+    <v-footer height="auto" fixed>
+      <v-card flat tile class="flex">
+        <v-card-text class="blue">
+          <strong class="subheading text-md-center">Get connected with us on social networks!</strong>
+          <v-spacer></v-spacer>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            icon
+            dark
+            class="mx-3"
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+        <v-card-actions class="grey lighten-2 justify-center">
+          &copy;2018 — <strong>Vuetify</strong>
+        </v-card-actions>
+      </v-card>
+    </v-footer>
   </div>
 </template>
 
@@ -29,7 +49,22 @@ export default {
   data () {
     return {
       imgUrl: 'static/BingWallpaper-2018-05-10.jpg',
-      drawer: false
+      drawer: false,
+      icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram'],
+      rows: [
+        {
+          title: 'Company Name',
+          children: ['Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit']
+        },
+        {
+          title: 'Products',
+          children: ['MDBootstrap', 'MDWordPress', 'BrandFlow', 'Bootstrap Angular']
+        },
+        {
+          title: 'Useful Links',
+          children: ['Your account', 'Become an Affiliate', 'Shipping Rates', 'Helper']
+        }
+      ]
     }
   }
 }
